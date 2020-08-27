c320ca77f6daf632f1dc32417a5139cabbadc427
niverton-felipe <nivertonfelipe355@gmail.com>
2020-08-27 00:30:05 -0300

    Adicionando arquivos de estruturas condicionais e laços de repetição

---
+++ Aula15_ControleDeFluxo.java
@@ -0,0 +1,26 @@
+public class Aula15_ControleDeFluxo {
+    public static void main(String[] args) {
+        int count = 1;
+        // executa a condição somente se o teste lógica retornar verdadeiro
+        while (count < 10) {
+            System.out.println(count++);
+        }
+
+        /*
+        * No Do While independente do teste lógico retornar verdadeiro,
+        * a condição é executada ao menos uma vez.
+        * */
+
+        do {
+            System.out.println("testando o do while");
+        }
+        while (count < 2);
+        //Testando o break dentro do laço for
+        for (int i = 0; i < 10; i++){
+            System.out.println("O valor de i é: " + i);
+            if (i == 5){
+                break;
+            }
+        }
+    }
+}
---
+++ Aula16_ExerciciosLacoRepeticao.java
@@ -0,0 +1,10 @@
+public class Aula16_ExerciciosLacoRepeticao {
+    // 1- Imprima todos os números pares de 0 a 100
+    public static void main(String[] args) {
+        int number = 0;
+        while(number <= 100){
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
